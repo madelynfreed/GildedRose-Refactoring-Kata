3cb1e23b0aa28c499cef66263df8ed26317d21e9
Madelyn Freed <madelyn.freed@gmail.com>
2021-05-19 19:58:03 -0400

    Extract method for expired item quality

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -21,21 +21,7 @@ class Shop {
         this.decrementSellInOfNonLegendaryItems(i);
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = 0;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
+        this.updateQualityOfExpiredItems(i)
       }
     }
 
@@ -68,6 +54,25 @@ class Shop {
 
   decrementSellInOfNonLegendaryItems(i) {
     this.items[i].sellIn = this.items[i].sellIn - 1;
+  }
+
+  updateQualityOfExpiredItems(i) {
+    if (this.items[i].name != 'Aged Brie') {
+      if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].quality > 0) {
+          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            this.items[i].quality = this.items[i].quality - 1;
+          }
+        }
+      } else {
+        this.items[i].quality = 0;
+      }
+    } else {
+      if (this.items[i].quality < 50) {
+        this.items[i].quality = this.items[i].quality + 1;
+      }
+    }
+
   }
 }
 
