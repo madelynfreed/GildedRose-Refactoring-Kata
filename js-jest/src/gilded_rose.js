304335327f93eb20fbffd030e43cf579b0cb1e33
Madelyn Freed <madelyn.freed@gmail.com>
2021-05-19 19:50:54 -0400

    Extract method for incrementing extraordinary items

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -15,21 +15,7 @@ class Shop {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         this.decrementQualityForOrdinaryItems(i)
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
+        this.incrementQualityOfExtraordinaryItems(i);
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
@@ -59,6 +45,23 @@ class Shop {
     if (this.items[i].quality > 0) {
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].quality = this.items[i].quality - 1;
+      }
+    }
+  }
+  incrementQualityOfExtraordinaryItems(i) {
+    if (this.items[i].quality < 50) {
+      this.items[i].quality = this.items[i].quality + 1;
+      if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].sellIn < 11) {
+          if (this.items[i].quality < 50) {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+        }
+        if (this.items[i].sellIn < 6) {
+          if (this.items[i].quality < 50) {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+        }
       }
     }
   }
