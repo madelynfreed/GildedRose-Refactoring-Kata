a57c6426ca8670e76ed5dbca131b048e8be25fe7
Madelyn Freed <madelyn.freed@gmail.com>
2021-05-19 20:24:43 -0400

    Move all item quality update methods into new object

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -18,7 +18,61 @@ class BetterItem {
   decrementSellInOfNonLegendaryItems() {
     this.item.sellIn = this.item.sellIn - 1;
   }
+  updateItemQuality() {
+    if (this.item.name != 'Aged Brie' && this.item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+      this.decrementQualityForOrdinaryItems()
+    } else {
+      this.incrementQualityOfExtraordinaryItems();
+    }
+    if (this.item.sellIn < 0) {
+      this.updateQualityOfExpiredItems()
+    }
+  }
+  decrementQualityForOrdinaryItems() {
+    if (this.item.quality > 0) {
+      if (this.item.name != 'Sulfuras, Hand of Ragnaros') {
+        this.item.quality = this.item.quality - 1;
+      }
+    }
+  }
+  incrementQualityOfExtraordinaryItems() {
+    if (this.item.quality < 50) {
+      this.item.quality = this.item.quality + 1;
+      if (this.item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.item.sellIn < 11) {
+          if (this.item.quality < 50) {
+            this.item.quality = this.item.quality + 1;
+          }
+        }
+        if (this.item.sellIn < 6) {
+          if (this.item.quality < 50) {
+            this.item.quality = this.item.quality + 1;
+          }
+        }
+      }
+    }
+  }
+
+  updateQualityOfExpiredItems() {
+    if (this.item.name != 'Aged Brie') {
+      if (this.item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.item.quality > 0) {
+          if (this.item.name != 'Sulfuras, Hand of Ragnaros') {
+            this.item.quality = this.item.quality - 1;
+          }
+        }
+      } else {
+        this.item.quality = 0;
+      }
+    } else {
+      if (this.item.quality < 50) {
+        this.item.quality = this.item.quality + 1;
+      }
+    }
+  }
+
 }
+
 
 class Shop {
   constructor(items=[]){
@@ -28,7 +82,7 @@ class Shop {
     for (let i = 0; i < this.items.length; i++) {
       const betterItem = new BetterItem(this.items[i])
 
-      this.updateItemQuality(i);
+      betterItem.updateItemQuality();
       betterItem.updateItemSellIn();
     }
 
