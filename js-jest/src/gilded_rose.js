812755e03edab72d38300ff52022084d11ef6662
Madelyn Freed <madelyn.freed@gmail.com>
2021-05-19 20:25:39 -0400

    Delete reduntant shop code

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -73,7 +73,6 @@ class BetterItem {
 
 }
 
-
 class Shop {
   constructor(items=[]){
     this.items = items;
@@ -87,59 +86,6 @@ class Shop {
     }
 
     return this.items;
-  }
-  decrementQualityForOrdinaryItems(i) {
-    if (this.items[i].quality > 0) {
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].quality = this.items[i].quality - 1;
-      }
-    }
-  }
-  incrementQualityOfExtraordinaryItems(i) {
-    if (this.items[i].quality < 50) {
-      this.items[i].quality = this.items[i].quality + 1;
-      if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].sellIn < 11) {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-        if (this.items[i].sellIn < 6) {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
-  }
-
-  updateQualityOfExpiredItems(i) {
-    if (this.items[i].name != 'Aged Brie') {
-      if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        this.items[i].quality = 0;
-      }
-    } else {
-      if (this.items[i].quality < 50) {
-        this.items[i].quality = this.items[i].quality + 1;
-      }
-    }
-  }
-
-  updateItemQuality(i) {
-    if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      this.decrementQualityForOrdinaryItems(i)
-    } else {
-      this.incrementQualityOfExtraordinaryItems(i);
-    }
-    if (this.items[i].sellIn < 0) {
-      this.updateQualityOfExpiredItems(i)
-    }
   }
 }
 
